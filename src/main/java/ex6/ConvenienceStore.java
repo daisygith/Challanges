9b8d43006d54cc0dd3735b5f63573875def8566f
daisygith <daisy.gith@gmail.com>
2024-01-15 10:23:56 +0100

    create colution to Convenience Store

---
+++ src/main/java/ex6/ConvenienceStore.java
@@ -0,0 +1,26 @@
+package ex6;
+/*
+ * quarter: 25 cents / $0.25
+ * dime: 10 cents / $0.10
+ * nickel: 5 cents / $0.05
+ * penny: 1 cent / $0.01
+ */
+
+public class ConvenienceStore {
+    public static boolean changeEnough(int[] change, double amountDue) {
+
+        double quarter = change[0] * 0.25;
+        double dime = change[1] * 0.10;
+        double nickel = change[2] * 0.05;
+        double penny = change[3] * 0.01;
+
+        double sumAll = quarter+dime+nickel+penny;
+
+        if(sumAll>= amountDue){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}

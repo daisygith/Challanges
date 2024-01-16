1f6df142c25bbdc112640eb8380b15b38431a766
daisygith <daisy.gith@gmail.com>
2024-01-16 10:15:48 +0100

    solved bracket problem

--- src/test/java/ex13/AdjacentNodesTest.java
+++ src/test/java/ex13/AdjacentNodesTest.java
@@ -15,87 +15,88 @@ public class AdjacentNodesTest {
 
         @BeforeClass
         public static void setup() {
-            graph1 = new int[][] {
-                    {0,1,0,0},
-                    {1,0,1,1},
-                    {0,1,0,1},
-                    {0,1,1,0}
+            graph1 = new int[][]{
+                    {0, 1, 0, 0},
+                    {1, 0, 1, 1},
+                    {0, 1, 0, 1},
+                    {0, 1, 1, 0}
             };
-            graph2 = new int[][] {
-                    {0,1,0,1,1},
-                    {1,0,1,0,0},
-                    {0,1,0,1,0},
-                    {1,0,1,0,1},
-                    {1,0,0,1,0}
+            graph2 = new int[][]{
+                    {0, 1, 0, 1, 1},
+                    {1, 0, 1, 0, 0},
+                    {0, 1, 0, 1, 0},
+                    {1, 0, 1, 0, 1},
+                    {1, 0, 0, 1, 0}
             };
             singleNode = new int[1][1];
             completeGraph = new int[5][5];
-            for (int i=0; i<5; i++) {
-                for (int j=0; j<5; j++) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
                     if (i != j) {
-                        completeGraph[i][j]=1;
+                        completeGraph[i][j] = 1;
                     }
                 }
             }
             noEdgeGraph = new int[5][5];
             largeGraph = new int[25000][25000];
             largeGraph[5000][20000] = 1;
-            largeGraph[20000][5000]=1;
+            largeGraph[20000][5000] = 1;
 
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test1() {
-            assertTrue(AdjacentNodes.adjacent(graph1,0,1));
+            assertTrue(AdjacentNodes.adjacent(graph1, 0, 1));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test2() {
-            assertFalse(AdjacentNodes.adjacent(graph2,0,2));
+            assertFalse(AdjacentNodes.adjacent(graph2, 0, 2));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test3() {
-            assertTrue(AdjacentNodes.adjacent(graph2,3,0));
+            assertTrue(AdjacentNodes.adjacent(graph2, 3, 0));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test4() {
-            assertFalse(AdjacentNodes.adjacent(graph2,4,1));
+            assertFalse(AdjacentNodes.adjacent(graph2, 4, 1));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test5() {
-            assertFalse(AdjacentNodes.adjacent(graph2,0,0));
+            assertFalse(AdjacentNodes.adjacent(graph2, 0, 0));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test6() {
-            assertFalse(AdjacentNodes.adjacent(singleNode,0,0));
+            assertFalse(AdjacentNodes.adjacent(singleNode, 0, 0));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test7() {
-            assertTrue(AdjacentNodes.adjacent(completeGraph,0,4));
+            assertTrue(AdjacentNodes.adjacent(completeGraph, 0, 4));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test8() {
-            assertFalse(AdjacentNodes.adjacent(completeGraph,2,2));
+            assertFalse(AdjacentNodes.adjacent(completeGraph, 2, 2));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test9() {
-            assertFalse(AdjacentNodes.adjacent(noEdgeGraph,4,0));
+            assertFalse(AdjacentNodes.adjacent(noEdgeGraph, 4, 0));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test10() {
-            assertTrue(AdjacentNodes.adjacent(largeGraph,5000,20000));
+            assertTrue(AdjacentNodes.adjacent(largeGraph, 5000, 20000));
         }
 
-        @Test(timeout=100)
+        @Test(timeout = 100)
         public void test11() {
-            assertFalse(AdjacentNodes.adjacent(largeGraph,21413,8079));
+            assertFalse(AdjacentNodes.adjacent(largeGraph, 21413, 8079));
         }
+    }
 }

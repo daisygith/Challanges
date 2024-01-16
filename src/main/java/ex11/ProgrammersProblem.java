b4f26cfbf871daa4fdb4110b320d1c4fb1b5eb4e
daisygith <daisy.gith@gmail.com>
2024-01-16 07:29:18 +0100

    create solution to ProgrammersProblem

--- src/main/java/ex11/ProgrammersProblem.java
+++ src/main/java/ex11/ProgrammersProblem.java
@@ -1,7 +1,24 @@
 package ex11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ProgrammersProblem {
     public static int programmers(int one, int two, int three) {
+        int result = 0;
+
+        List<Integer> listNumber = new ArrayList<>();
+        listNumber.add(one);
+        listNumber.add(two);
+        listNumber.add(three);
+
+        int max = Collections.max(listNumber);
+        int min = Collections.min(listNumber);
+
+        result = max - min;
+
+        return result;
 
     }
 }

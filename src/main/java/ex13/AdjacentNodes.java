a7d822baa6a480450ad47327c4b00db6ac5f3d51
daisygith <daisygith@users.noreply.github.com>
2024-01-21 14:55:22 +0100

    add solution to AdjacentNodes

--- src/main/java/ex13/AdjacentNodes.java
+++ src/main/java/ex13/AdjacentNodes.java
@@ -2,7 +2,12 @@ package ex13;
 
 public class AdjacentNodes {
     public static boolean adjacent(int[][] graph, int node1, int node2) {
-        return true;
 
+
+        if(graph[node1][node2] == 1 && graph[node2][node1] == 1){
+            return true;
+        } else{
+            return false;
+        }
     }
 }

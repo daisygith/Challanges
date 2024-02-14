c521d83ade34ec43e870799d4eed6103777d911a
daisygith <daisygith@users.noreply.github.com>
2024-02-14 11:07:07 +0100

    create recursion solution to BinarySearch

--- src/main/java/ex33/BinarySearch.java
+++ src/main/java/ex33/BinarySearch.java
@@ -2,21 +2,35 @@ package ex33;
 
 public class BinarySearch {
     public static boolean binarySearch(int arr[], int left, int right, int elem) {
+        return binarySearchRec(arr, left, right, elem);
+//        while(left<=right){
+//            int middle = (left+right)/2;
+//            if (arr[middle] <elem){
+//                left=middle+1;
+//            } else if (arr[middle] > elem) {
+//                right=middle-1;
+//            }
+//            else {
+//                return true;
+//            }
+//        }
+//
+//        return false;
 
-        while(left<=right){
+    }
+    public static boolean binarySearchRec(int arr[], int left, int right, int elem) {
+
+        if (left <= right) {
             int middle = (left+right)/2;
             if (arr[middle] <elem){
-                left=middle+1;
+                return binarySearchRec(arr, middle+1, right, elem);
             } else if (arr[middle] > elem) {
-                right=middle-1;
+                return binarySearchRec(arr, left, middle-1, elem);
             }
             else {
                 return true;
             }
         }
-
         return false;
-
     }
-
 }

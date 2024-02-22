e510dfd44774cc7d1390509705361f9f8e787042
daisygith <daisygith@users.noreply.github.com>
2024-02-22 07:53:55 +0100

    add solution to PinNumber

--- src/main/java/ex35/PinNumber.java
+++ src/main/java/ex35/PinNumber.java
@@ -6,17 +6,12 @@ import java.util.regex.Pattern;
 
 public class PinNumber {
     public static boolean validate(String pin) {
-        Pattern pattern1 = Pattern.compile("\\S+\\d\\w[^a-z]");
-//        Pattern pattern2 = Pattern.compile("[\\w\\d][{6}]");
-//        Pattern pattern1 = Pattern.compile("(?=[.\\w\\d][{4}])");
-//        Pattern pattern2 = Pattern.compile("(?=[.\\w\\d][{6}])");
-        Matcher matcher1 = pattern1.matcher(pin);
-//        Matcher matcher2 = pattern2.matcher(pin);
-
-        if((pin.length() == 6 || pin.length() == 4)){
-            return matcher1.find();
-        }
-        return false;
+//        Pattern pattern1 = Pattern.compile("^\\d{4}(\\d{2})?$");
+//
+//        Matcher matcher1 = pattern1.matcher(pin);
+//
+//            return matcher1.find();
+            return pin.matches("^\\d{4}(\\d{2})?$");
 
     }
 }

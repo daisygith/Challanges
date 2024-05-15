3f005a01b096d0401be9cc13d35eacaad075d26b
daisygith <daisygith@users.noreply.github.com>
2024-05-15 08:49:45 +0200

    added Recursion solution

--- src/main/java/ex42/inputFactorial.java
+++ src/main/java/ex42/inputFactorial.java
@@ -2,21 +2,33 @@ package ex42;
 
 public class inputFactorial {
     public static boolean isFactorial(int n) {
-        int m = n;
-        int pom = 1;
-        for (int i = 1; i <= n; i++){
-            if (i<=n){
-                pom = pom * i;
-                n = n/i;
-            }
-        }
+//        int m = n;
+//        int pom = 1;
+//        for (int i = 1; i <= n; i++){
+//            if (i<=n){
+//                pom = pom * i;
+//                n = n/i;
+//            }
+//        }
+//
+//        if(pom == m){
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
 
-        if(pom == m){
-            return true;
-        }
-        else {
-            return false;
-        }
-
+        //        Recursion
+        return silnia(n, 1, 1);
     }
+
+    public static boolean silnia(int n, int i, int sum){
+        if(n==0 || n == sum) return true;
+        else if (i > n || n < sum) return false;
+        else {
+            sum *= i;
+            return silnia(n, ++i, sum);
+        }
+    }
+
 }

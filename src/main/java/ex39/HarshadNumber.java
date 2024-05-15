9c78841541d59845328f5d8087e7bfced24be72f
daisygith <daisygith@users.noreply.github.com>
2024-05-15 08:49:30 +0200

    added Recursion solution

--- src/main/java/ex39/HarshadNumber.java
+++ src/main/java/ex39/HarshadNumber.java
@@ -4,32 +4,50 @@ import java.util.ArrayList;
 
 public class HarshadNumber {
     public static boolean isHarshad(int n) {
-        int m = n;
-        int count =0;
-
-        ArrayList<Integer> list = new ArrayList<>();
-        while (n > 0) {
-            list.add(n%10);
-            n/=10;
-        }
-
-        for (int i=0; i<=list.size()-1; i++){
-            count = list.get(i) + count;
-        }
-
-        if(m % count == 0){
-            return true;
-        } else {
-            return false;
-        }
-//        return isHarshadRec(n);
-//    }
-
-//    public static boolean isHarshadRec(int n) {
-//        ArrayList<Integer> list = new ArrayList<>();
-//        if (n != 0) {
-//            list.add(n % 10);
+//        int m = n;
+//        int count =0;
 //
+//        ArrayList<Integer> list = new ArrayList<>();
+//        while (n > 0) {
+//            list.add(n%10);
+//            n/=10;
+//        }
+//
+//        for (int i=0; i<=list.size()-1; i++){
+//            count = list.get(i) + count;
+//        }
+//
+//        if(m % count == 0){
+//            return true;
+//        } else {
+//            return false;
+//        }
+
+        return isHarshadRec(n,n,0);
+    }
+
+    public static boolean isHarshadRec(int originalN, int n, int count) {
+        if (n < 10) {
+            count += n;
+            return originalN % count == 0;
+        } else {
+            count += n % 10;
+            return isHarshadRec(originalN, n / 10, count);
+        }
+//        //        int count =0;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        if (n >0) {
+//            list.add(n % 10);
+//            n /= 10;
+//        } if (n<=0) {
+//            for (int i=0; i<=list.size()-1; i++){
+//            count = list.get(i) + count;
+//            }
+//            return isHarshadRec(n,count);
+//        } else if ( %count==0){
+//            return true;
+//        } else {
+//            return false;
 //        }
     }
 }
